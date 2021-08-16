4f0bd30e34ccf90b3f69fe98c0b07b8f64d9016c
Priscila Ribeiro Monteiro <pririmonteiro@Priscilas-MBP.attlocal.net>
2021-08-16 14:16:21 -0700

    first commit

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,16 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useParams, useRouteMatch, NavLink, Route, Switch } from 'react-router-dom';
 
 export default function Movie(props) {
   const [movie, setMovie] = useState();
 
-  let id = 1;
+  const { movies } = props;
+  const { movieId } = useParams();
+  const { path, url } = useRouteMatch();
   // Change ^^^ that line and use a hook to obtain the :id parameter from the URL
 
+  const id = movies.find(movie => movie.id === parseInt(movieId))
+  
   useEffect(() => {
     axios
-      .get(`http://localhost:5000/api/movies/${id}`) // Study this endpoint with Postman
+      .get(`http://localhost:5000/api/movies/${movieId}`) // Study this endpoint with Postman
       .then(response => {
+        setMovie(response.data)
         // Study this response with a breakpoint or log statements
         // and set the response data as the 'movie' slice of state
       })
@@ -41,12 +47,13 @@ export default function Movie(props) {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
+        
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
           </div>
         ))}
+
       </div>
       <div className="save-button">Save</div>
     </div>
